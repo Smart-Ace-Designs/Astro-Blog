5798c949981bc7a87d468fc1ff12e4a38a858148
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2024-05-04 23:38:45 -0500

    Allow Netlify CDN again

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,5 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: netlify({
-    imageCDN: false,
-  }),
+  adapter: netlify(),
 });
