6c2bfdd1de39d803ccc9185e70b1bd884d8d8972
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2024-12-03 22:25:08 -0600

    Updates project to Astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
+// @ts-check
 import { defineConfig } from "astro/config";
+
 import tailwind from "@astrojs/tailwind";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  legacy: {
+    collections: true,
+  },
   integrations: [tailwind()],
   output: "server",
   adapter: netlify(),
