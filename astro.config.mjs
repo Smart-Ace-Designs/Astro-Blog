056f9066cc9d3568f1f1f1a0e2237e4053028c17
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-02-04 14:58:41 -0600

    Converts to Tailwind CSS v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   legacy: {
     collections: true,
   },
-  integrations: [tailwind()],
+  integrations: [],
   output: "server",
   adapter: netlify(),
 });
